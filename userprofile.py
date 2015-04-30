462add6b6f707009e0b3a22bc3005384e66c7819
Xuesong Gao <highcedar@hotmail.com>
2015-04-30 10:17:38 +0800

    Support Chinese language

--- userprofile.py
+++ userprofile.py
@@ -7,15 +7,15 @@ from wordcloud import WordCloud, STOPWORDS
 d = path.dirname(__file__)
 
 # Read the whole text.
-text = open(path.join(d, 'alice.txt')).read()
+text = open(path.join(d, 'hot_key.txt')).read()
 
 # read the mask image
 # taken from
 # http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg
 alice_mask = imread(path.join(d, "alice_mask.png"))
 
-wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
-               stopwords=STOPWORDS.add("said"))
+wc = WordCloud(font_path="simhei.ttf", background_color="white", max_words=2000, mask=alice_mask,
+               stopwords=STOPWORDS.add("Qq"))
 # generate word cloud
 wc.generate(text)
 
